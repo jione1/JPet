4d9187508bc59c015e07b6972d7ef0b21aa592b0
shineeseo <20shinee@gmail.com>
2018-04-13 14:40:51 +0900

    third commit

---
+++ jpetstore/src/main/java/com/example/jpetstore/service/P2PService.java
@@ -0,0 +1,27 @@
+package com.example.jpetstore.service;
+
+public interface P2PService {
+	//p2p
+
+	void insertItem(Item item);
+	//판매자가 작성한 p2p에 있는 item 정보를 저장한다. 
+
+	void insertP2P(P2P p2p);
+	//사용자가 입력한 p2p post의 정보를 삽입한다. 
+
+	//void updateQuantity(int itemId, int quantity);
+	//사용자가 주문/ 주문취소 했을 경우 item의 수량을 변경한다. 
+
+	P2P getP2PList(String userId);
+	//userId에 맞는 p2p리스트를 받아와서 판매자페이지를 구성한다. 
+
+	Item getItem(String itemId, String userId); 
+	//userId가 있으면 판매자 아이템을 가져온다. 
+
+	void deleteItem(int itemId);
+	//cascade를 이용해서 연계된 p2p도 지운다. 
+
+	void updateP2P(Item itemId);
+	//판매자가 아이템 정보를 수정하였을 경우 호출된다. 
+
+}
---
+++ jpetstore/src/main/java/com/example/jpetstore/service/TendencyServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.jpetstore.service;
+
+public class TendencyServiceImpl implements TendencyService {
+
+	@Override
+	public String getTendencyResult(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
